b0feeedbc54717dafc2531b1db2bfc943168a1b8
hooman <hham.group12@gmail.com>
2020-01-15 17:33:48 +0330

    comment added

--- HMS/sprint_1/models.py
+++ HMS/sprint_1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from datetime import datetime
-
+# a expense model
 class Expense(models.Model):
     title = models.CharField(max_length=20)
     amount = models.BigIntegerField()
@@ -12,7 +12,7 @@ class Expense(models.Model):
         return "{}-{}-{}" .format(self.title,self.amount,self.date)
     
 
-
+#income model
 class Income(models.Model):
     title = models.CharField(max_length=20)
     amount = models.BigIntegerField()
@@ -21,7 +21,7 @@ class Income(models.Model):
     def __str__(self):
         return "{}-{}-{}" .format(self.title,self.amount,self.date)
     
-
+#Employee model
 class Employee(models.Model):
     full_name = models.CharField(max_length=30)
     national_id = models.CharField(max_length=40)
@@ -29,7 +29,8 @@ class Employee(models.Model):
     income = models.BigIntegerField()
     last_payment = models.DateTimeField()
     paid = models.BooleanField()
-    def payment(self):
+    def payment(self): 
+        #check that is the employee paid or not if not change a value in model
         if abs((self.last_payment-datetime.now).days) > 30:
             self.paid = False
     def __str__(self):
