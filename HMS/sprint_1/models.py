c885dc6d727ae53addac8cee70eb73b32359ec61
hooman <hham.group12@gmail.com>
2020-01-15 17:18:58 +0330

    i added 3 models for Incomes Expences and Employess it didnt test and didnt commented but i will be complete

--- HMS/sprint_1/models.py
+++ HMS/sprint_1/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+class Expense(models.Model):
+    title = models.CharField(max_length=20)
+    amount = models.BigIntegerField()
+    description = models.TextField()
+    date  = models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return "{}-{}-{}" .format(self.title,self.amount,self.date)
+    
+
+
+class Income(models.Model):
+    title = models.CharField(max_length=20)
+    amount = models.BigIntegerField()
+    description = models.TextField()
+    date  = models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return "{}-{}-{}" .format(self.title,self.amount,self.date)
+    
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=30)
+    national_id = models.CharField(max_length=40)
+    date_hired = models.DateTimeField(default=datetime.now)
+    income = models.BigIntegerField()
+    last_payment = models.DateTimeField()
+    paid = models.BooleanField()
+    def payment(self):
+        if abs((self.last_payment-datetime.now).days) > 30:
+            self.paid = False
+    def __str__(self):
+        return self.full_name
+    
