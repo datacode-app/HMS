cf9710418b0ac2deb7170c61049119269f2ad2b1
hooman <hham.group12@gmail.com>
2020-01-15 18:03:07 +0330

    testing but an error in substract two datetimes i will fix it

--- HMS/sprint_1/models.py
+++ HMS/sprint_1/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from datetime import datetime
+from django.utils import timezone
 # a expense model
 class Expense(models.Model):
     title = models.CharField(max_length=20)
     amount = models.BigIntegerField()
     description = models.TextField()
-    date  = models.DateTimeField(default=datetime.now)
+    date  = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return "{}-{}-{}" .format(self.title,self.amount,self.date)
     
@@ -17,7 +17,7 @@ class Income(models.Model):
     title = models.CharField(max_length=20)
     amount = models.BigIntegerField()
     description = models.TextField()
-    date  = models.DateTimeField(default=datetime.now)
+    date  = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return "{}-{}-{}" .format(self.title,self.amount,self.date)
     
@@ -25,14 +25,17 @@ class Income(models.Model):
 class Employee(models.Model):
     full_name = models.CharField(max_length=30)
     national_id = models.CharField(max_length=40)
-    date_hired = models.DateTimeField(default=datetime.now)
+    date_hired = models.DateTimeField(default=timezone.now)
     income = models.BigIntegerField()
     last_payment = models.DateTimeField()
     paid = models.BooleanField()
-    def payment(self): 
+    def save(self):
         #check that is the employee paid or not if not change a value in model
-        if abs((self.last_payment-datetime.now).days) > 30:
-            self.paid = False
+        print(self.last_payment)
+        print(timezone.now-self.last_payment)
+        
+        """if self.last_payment - datetime.now()).days > 30:
+            self.paid = False"""
     def __str__(self):
         return self.full_name
     
