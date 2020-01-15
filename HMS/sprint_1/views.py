1e0f96b42c59a8f11576abd366bb87464f2403e5
hooman <hham.group12@gmail.com>
2020-01-15 23:26:09 +0330

    second view writen about the pay of a employee

--- HMS/sprint_1/views.py
+++ HMS/sprint_1/views.py
@@ -5,6 +5,7 @@ from django.core import serializers
 from . import models
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+import datetime
 # Create your views here.
 @csrf_exempt
 def get_unpaid(request):
@@ -15,3 +16,18 @@ def get_unpaid(request):
     else:
         return JsonResponse({"error":"permission denied"},encoder=JSONEncoder)
     
+@csrf_exempt
+def pay(request):
+    if "3" in request.user.get_group_permissions(): #check the permission
+        emp = models.Employee.objects.get(national_id = request.POST["national_id"]) #query employee
+        models.Expense.objects.create(
+            title = "pay employee {}" .format(emp.full_name),
+            amount = emp.Income,
+            description = request.POST["description"]
+        )#create expense
+        emp.paid = True
+        emp.last_payment = datetime.datetime.now
+        emp.save() #update employee data
+        return JsonResponse({"result":"payment successfully done!"},encoder=JSONEncoder)
+    else:
+        return JsonResponse({"error":"permission denied",encoder=JSONEncoder})
