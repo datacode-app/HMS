583bc697bf96520f13916b308c91b40abbbda7be
hooman <hham.group12@gmail.com>
2020-01-15 21:28:01 +0330

    pep-8 standard - a little debug

--- HMS/sprint_1/models.py
+++ HMS/sprint_1/models.py
@@ -1,47 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 from django.utils import timezone
 # a expense model
 class Expense(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length = 20)
     amount = models.BigIntegerField()
     description = models.TextField()
-    date  = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(default = timezone.datetime.now)
     def __str__(self):
         return "{}-{}-{}" .format(self.title,self.amount,self.date)
-    
-
 #income model
 class Income(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length = 20)
     amount = models.BigIntegerField()
     description = models.TextField()
-    date  = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(default = timezone.datetime.now)
     def __str__(self):
-        return "{}-{}-{}" .format(self.title,self.amount,self.date)
-    
+        return "{}-{}-{}" .format(self.title,self.amount,self.date) 
 #Employee model
 class Employee(models.Model):
-    full_name = models.CharField(max_length=30)
-    national_id = models.CharField(max_length=40)
-    date_hired = models.DateTimeField(default=timezone.datetime.now)
+    full_name = models.CharField(max_length = 30)
+    national_id = models.CharField(max_length = 40)
+    date_hired = models.DateTimeField(default = timezone.datetime.now)
     income = models.BigIntegerField()
     last_payment = models.DateTimeField()
     paid = models.BooleanField()
-
-        #check that is the employee paid or not if not change a value in model
+    #check that is the employee paid or not if not change a value in model
     def save(self, *args, **kwargs): 
         now = timezone.datetime.now()
         ptime = self.last_payment
         pdelta = timezone.datetime(ptime.year,ptime.month,ptime.day,ptime.hour,ptime.minute,ptime.microsecond)
         diff = now - pdelta
-        print(diff.days)
-        
         if diff.days > 30 :
-            self.paid = False
-            
+            self.paid = False    
         super(Employee, self).save(*args, **kwargs) 
     def __str__(self):
         return self.full_name
-    
